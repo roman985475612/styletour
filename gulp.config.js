1b27d189311d587e99814a27abda09515c0048dd
Roman <roman985475612@gmail.com>
2021-07-04 14:49:29 +0300

    Chapter 12 completed

--- gulp.config.js
+++ gulp.config.js
@@ -2,11 +2,10 @@ module.exports = {
     SRC_PATH: 'src',
     DIST_PATH: 'dist',
     STYLES_LIBS: [
-        './node_modules/bootstrap/dist/css/bootstrap.min.css',
         './src/libs/fontawesome-all.min.css'
     ],
     JS_LIBS: [
-        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
     ],
 
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,30 @@
-const { src, dest, task, series, watch, parallel } = require('gulp')
+const { src, dest, task, series, watch, parallel }  = require('gulp')
 const { DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS } = require('./gulp.config')
-const rm = require('gulp-rm')
-const sass = require('gulp-sass')
-const concat = require('gulp-concat')
-const browserSync = require('browser-sync').create()
-const reload = browserSync.reload
-const sassGlob = require('gulp-sass-glob')
-const autoprefixer = require('gulp-autoprefixer')
-const px2rem = require('gulp-smile-px2rem')
-const gcmq = require('gulp-group-css-media-queries')
-const cleanCss = require('gulp-clean-css')
-const sourcemaps = require('gulp-sourcemaps')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
-const svgo = require('gulp-svgo')
-const svgSprite = require('gulp-svg-sprite')
-const gulpIf = require('gulp-if')
-const webp = require('gulp-webp')
-const imagemin = require('gulp-imagemin')
-
+const rm            = require('gulp-rm')
+const sass          = require('gulp-sass')
+const concat        = require('gulp-concat')
+const browserSync   = require('browser-sync').create()
+const reload        = browserSync.reload
+const autoprefixer  = require('gulp-autoprefixer')
+const gcmq          = require('gulp-group-css-media-queries')
+const cleanCss      = require('gulp-clean-css')
+const sourcemaps    = require('gulp-sourcemaps')
+const babel         = require('gulp-babel')
+const uglify        = require('gulp-uglify')
+const svgo          = require('gulp-svgo')
+const svgSprite     = require('gulp-svg-sprite')
+const gulpIf        = require('gulp-if')
+const webp          = require('gulp-webp')
+const imagemin      = require('gulp-imagemin')
+const env           = process.env.NODE_ENV
 
 sass.compiler = require('node-sass')
-
-const env = process.env.NODE_ENV
 
 task('clean', () => {
     return src([
         `${DIST_PATH}/**/*`, 
         `!${DIST_PATH}/img/**/*`,
+        `!${DIST_PATH}/fonts/**/*`,
         `!${DIST_PATH}/webfonts/**/*`,
     ], {read: false})
         .pipe(rm())
@@ -46,18 +43,26 @@ task('copy:html', () => {
         .pipe(reload({stream: true}))
 })
 
-task('copy:fonts', () => {
+task('copy:webfonts', () => {
     return src(`${SRC_PATH}/webfonts/**/*`)
         .pipe(dest(`${DIST_PATH}/webfonts`))
 })
 
+task('copy:fonts', () => {
+    return src(`${SRC_PATH}/fonts/**/*`)
+        .pipe(dest(`${DIST_PATH}/fonts`))
+})
+
+task('copy:css', () => {
+    return src([...STYLES_LIBS])
+        .pipe(dest(`${DIST_PATH}/css`))
+})
+
 task('scss', () => {
-    return src([...STYLES_LIBS, `${SRC_PATH}/scss/main.scss`])
+    return src(`${SRC_PATH}/scss/main.scss`)
         .pipe(gulpIf(env === 'dev', sourcemaps.init()))
         .pipe(concat('main.min.scss'))
-        .pipe(sassGlob())
         .pipe(sass().on('error', sass.logError))
-        // .pipe(px2rem())
         .pipe(gulpIf(env === 'prod', autoprefixer({
             browsers: ['last 2 versions'],
             cascade: true
@@ -69,8 +74,13 @@ task('scss', () => {
         .pipe(reload({stream: true}))
 })
 
+task('copy:js', () => {
+    return src([...JS_LIBS])
+        .pipe(dest(`${DIST_PATH}/js`))
+})
+
 task('js', () => {
-    return src([...JS_LIBS, `${SRC_PATH}/js/*.js`])
+    return src(`${SRC_PATH}/js/*.js`)
         .pipe(gulpIf(env === 'dev', sourcemaps.init()))
         .pipe(concat('main.min.js', {newLine: ';'}))
         .pipe(gulpIf(env === 'prod', babel({
@@ -130,10 +140,17 @@ task('watch', () => {
     watch(`./${SRC_PATH}/*.html`, series('copy:html'))
 })
 
+task('static',
+    series(
+        'clean:all',
+        parallel('img', 'img:webp', 'copy:fonts', 'copy:webfonts')
+    )
+)
+
 task('default', 
     series(
         'clean', 
-        parallel('copy:html', 'scss', 'js'), 
+        parallel('copy:html', 'copy:css', 'scss', 'copy:js', 'js'), 
         parallel('watch', 'server')
     )
 )
@@ -141,6 +158,16 @@ task('default',
 task('build', 
     series(
         'clean', 
-        parallel('copy:html', 'scss', 'js', 'img', 'img:webp', 'copy:fonts')
+        parallel(
+            'copy:html', 
+            'copy:css', 
+            'scss', 
+            'copy:js', 
+            'js', 
+            'img', 
+            'img:webp', 
+            'copy:fonts', 
+            'copy:webfonts'
+        )
     ) 
 )
