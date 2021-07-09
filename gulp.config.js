b1653589674f23c4f83272a57d642eb2983ea45f
Roman <roman985475612@gmail.com>
2021-07-09 12:39:11 +0300

    Project completed

--- gulp.config.js
+++ gulp.config.js
@@ -2,10 +2,12 @@ module.exports = {
     SRC_PATH: 'src',
     DIST_PATH: 'dist',
     STYLES_LIBS: [
-        './src/libs/fontawesome-all.min.css'
+        './src/libs/fontawesome-all.min.css',
+        './src/libs/OwlCarousel2/dist/assets/owl.carousel.min.css'
     ],
     JS_LIBS: [
-        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        './node_modules/jquery/dist/jquery.min.js',
+        './src/libs/OwlCarousel2/dist/owl.carousel.min.js'
     ],
-
 }
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $( '.owl-carousel' ).owlCarousel({
+        margin: 10,
+        loop: true,
+        autoWidth: true,
+        items: 10,
+        nav: true
+    })
+})
